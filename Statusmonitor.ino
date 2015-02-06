8c0b218a005ba033a700b1ce9c3582cb82b3462a
Hartvik Line <hartvik.line@gmail.com>
2015-02-06 17:38:00 +0100

    Added methodes and startet to code

--- Statusmonitor.ino
+++ Statusmonitor.ino
@@ -1,13 +1,34 @@
 void setup()
 {
+	volatile double initial_watt_hours = 500
+	volatile int humidity_ararm_value = 30 //Needs to be updatet to sensable value
 
-  /* add setup code here */
-
+	watt_hours_left = read_from_eeprom();
 }
+
 
 void loop()
 {
+	boolean humidity_alarm = check_humidity();
+	boolean temp_alarm = check_temp();
+	double voltage_drop = check_voltage_drop();
+	akkumulate_ampere(coltage_drop);
 
-  /* add main program code here */
+	if (resetswitch_pushed){
+		watt_hours_left = initial_watt_hours;
+	}
+	//If time > 30 sek
+	//Save 
+}
 
+boolead check_humidity(){
+	if (humidity.get()< humidity_ararm_value){
+		return false;
+	}
+	return true;
+}
+
+double read_from_eeprom(){
+
+	return
 }
